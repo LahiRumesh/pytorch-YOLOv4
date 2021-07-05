992f1c2a758d1516947ecd2423b7c75692529af1
LahiRumesh <lahrumesh28@gmail.com>
2021-07-05 23:19:21 +0530

    model configs

--- cfg.py
+++ cfg.py
@@ -18,13 +18,13 @@ _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 Cfg = EasyDict()
 
-Cfg.use_darknet_cfg = True
+Cfg.use_darknet_cfg = False
 Cfg.cfgfile = os.path.join(_BASE_DIR, 'cfg', 'yolov4.cfg')
 
-Cfg.batch = 64
-Cfg.subdivisions = 16
-Cfg.width = 608
-Cfg.height = 608
+Cfg.batch = 4
+Cfg.subdivisions = 1
+Cfg.width = 416
+Cfg.height = 416
 Cfg.channels = 3
 Cfg.momentum = 0.949
 Cfg.decay = 0.0005
@@ -35,8 +35,8 @@ Cfg.hue = .1
 
 Cfg.learning_rate = 0.00261
 Cfg.burn_in = 1000
-Cfg.max_batches = 500500
-Cfg.steps = [400000, 450000]
+Cfg.max_batches = 20000
+Cfg.steps = [16000, 18000]
 Cfg.policy = Cfg.steps
 Cfg.scales = .1, .1
 
@@ -45,15 +45,15 @@ Cfg.mosaic = 1
 
 Cfg.letter_box = 0
 Cfg.jitter = 0.2
-Cfg.classes = 80
+Cfg.classes = 10
 Cfg.track = 0
 Cfg.w = Cfg.width
 Cfg.h = Cfg.height
 Cfg.flip = 1
 Cfg.blur = 0
 Cfg.gaussian = 0
-Cfg.boxes = 60  # box num
-Cfg.TRAIN_EPOCHS = 300
+Cfg.boxes = 10  # box num
+Cfg.TRAIN_EPOCHS = 100
 Cfg.train_label = os.path.join(_BASE_DIR, 'data', 'train.txt')
 Cfg.val_label = os.path.join(_BASE_DIR, 'data' ,'val.txt')
 Cfg.TRAIN_OPTIMIZER = 'adam'
@@ -75,4 +75,4 @@ Cfg.TRAIN_TENSORBOARD_DIR = os.path.join(_BASE_DIR, 'log')
 
 Cfg.iou_type = 'iou'  # 'giou', 'diou', 'ciou'
 
-Cfg.keep_checkpoint_max = 10
+Cfg.keep_checkpoint_max = 5
